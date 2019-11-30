9b4fdc26813aef371a24c0becc4838a003cdbbf2
ucarer <adamlin@ucarer.com.tw>
2019-11-30 15:28:48 +0800

    change login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
         $user = \Illuminate\Support\Facades\Auth::user();
 
         if(empty($user)){
-            return $token;
+            return "";
         }
 
         $data = array();
